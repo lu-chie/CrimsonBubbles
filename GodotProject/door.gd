71a8eefc499ce1c76b3b93fc5100877a708d9fb7
GalGaleno <galgaleno.art@gmail.com>
2025-01-25 08:58:35 -0400

    portais
    - sisteminha dos portais e cena do mundo

---
+++ GodotProject/door.gd
@@ -0,0 +1,30 @@
+extends Area
+
+export(String, FILE, "*.tscn") var target_scene_path
+export(NodePath) var spawn_point_path
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+func _on_Area_body_entered(body):
+	if body.name == "Player":
+		move_player_to_spawn(body)
+	
+	pass # Replace with function body.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func move_player_to_spawn(player):
+	if spawn_point_path:
+		var spawn_point = get_node(spawn_point_path)
+		if spawn_point:
+			player.global_transform.origin = spawn_point.global_transform.origin
+			
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+
